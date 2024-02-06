3688b5c58bfc271322750f31f08f35097ff000ee
Acrylic125 <acrylic125email@gmail.com>
2024-02-07 07:22:25 +0800

    fix: stupid build error hack

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
+// https://github.com/crxjs/chrome-extension-tools/issues/846
+const viteManifestHackIssue846: any = {
+  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+  name: "manifestHackIssue846",
+  renderCrxManifest(_manifest, bundle) {
+    bundle["manifest.json"] = bundle[".vite/manifest.json"];
+    bundle["manifest.json"].fileName = "manifest.json";
+    delete bundle[".vite/manifest.json"];
+  },
+};
+
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [viteManifestHackIssue846, crx({ manifest })],
 });
