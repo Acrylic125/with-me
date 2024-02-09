14afa5109e262c720f161dea07faf61122fa4cca
Acrylic125 <acrylic125email@gmail.com>
2024-02-10 06:45:20 +0800

    feat: add click listener !wip

--- src/content/highlighter.ts
+++ src/content/highlighter.ts
@@ -7,20 +7,34 @@ const texts = {
   },
 };
 
-function highlightText(keyword: string) {
-  const textNode = document.querySelector("*");
+function highlightText(keyword: string, description: string) {
+  const textNode = document.querySelector("body *");
   if (textNode && textNode.textContent && textNode.textContent.trim()) {
     // Highlight logic using DOM manipulation
+    // textNode.textContent = "";
+    // const ele = document.createElement("span");
+    // ele.classList.add("unset", "highlighted-elemented");
+    // ele.innerText = keyword;
+
     textNode.innerHTML = textNode.innerHTML.replace(
       // Regular expression for case-insensitive search
       new RegExp(keyword, "gi"),
-      `<span class="highlighted-elemented">${keyword}</span>`
+      `<span class="highlighted-elemented" data-highlighted-element-keyword="${keyword}">${keyword}</span>`
       // `<span style='background-color: yellow'>${keyword}</span>`
     );
   }
+  const highlighted = document.querySelectorAll(`.highlighted-elemented[data-highlighted-element-keyword="${keyword}"]`);
+  highlighted.forEach((element) => {
+    if (!(element instanceof HTMLSpanElement)) {
+      return;
+    }
+    element.onclick = () => {
+      console.log("clicked");
+    };
+  });
 }
 
-highlightText("build");
+highlightText("build", "The most common word in English");
 
 // chrome.action.onClicked.addListener(() => {
 //   //   // Retrieve highlight keyword from storage or options page
